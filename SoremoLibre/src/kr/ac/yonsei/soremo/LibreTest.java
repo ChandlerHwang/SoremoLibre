73f15a29013d7ceb0921aba88fe75a65916297ec
Friday <arsenal@yonsei.ac.kr>
2018-12-04 13:35:35 +0900

    Commencing.

---
+++ SoremoLibre/src/kr/ac/yonsei/soremo/LibreTest.java
@@ -0,0 +1,12 @@
+package kr.ac.yonsei.soremo;
+
+public class LibreTest {
+
+	public LibreTest() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Libre office test for SoReMo project");
+	}
+
+}
